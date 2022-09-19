37ff9e53f31614840ce594302421d94c063e1941
Kota <maedakoutaa@gmail.com>
2022-09-20 08:59:27 +0900

    Initial Commit

---
+++ MiniApp99-SwiftUIWithStoryBoard/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MiniApp99-SwiftUIWithStoryBoard
+//
+//  Created by 前田航汰 on 2022/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MiniApp99-SwiftUIWithStoryBoard/MiniApp99_SwiftUIWithStoryBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniApp99_SwiftUIWithStoryBoardApp.swift
+//  MiniApp99-SwiftUIWithStoryBoard
+//
+//  Created by 前田航汰 on 2022/09/20.
+//
+
+import SwiftUI
+
+@main
+struct MiniApp99_SwiftUIWithStoryBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
